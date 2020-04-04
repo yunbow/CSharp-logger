80f9bc6b9e5de39f2f5af0797c76cce3787c7e0c
yun2 <yun2panda@gmail.com>
2020-04-04 23:29:24 +0900

    init

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger log = Logger.GetInstance();
+            log.Error("error log.");
+            log.Warn("warn log.");
+            log.Info("info log.");
+            log.Debug("debug log.");
+        }
+    }
+}
